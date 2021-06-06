47e29919f4048a33bfc993c5d9938ff37d49d4e1
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-06 11:26:17 +0600

    MainActivity Done

--- app/src/main/java/com/example/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.java
@@ -1,14 +1,162 @@
 package com.example.qrcodescanner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
 
+import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListenableFuture cameraProviderFuture;
+    private ExecutorService cameraExecutor;
+    private PreviewView previewView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        previewView = findViewById(R.id.previewview);
+        this.getWindow().setFlags(1024, 1024);
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider processCameraProvider = (ProcessCameraProvider)cameraProviderFuture.get();
+                    bindpreview(processCameraProvider);
+                }catch (ExecutionException e){
+                    e.printStackTrace();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor());
+
     }
+
+    private void bindpreview(ProcessCameraProvider processCameraProvider) {
+
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        ImageCapture imageCapture = new ImageCapture();
+        processCameraProvider.unbindAll();
+        processCameraProvider.bindToLifecycle(this, cameraSelector,preview,imageCapture,imageAnalysis);
+
+    }
+    public class MyImageAnalyzer implements ImageAnalysis.Analyzer{
+        private FragmentManager fragmentManager;
+
+
+        public MyImageAnalyzer(FragmentManager fragmentManager){
+            this.fragmentManager = fragmentManager;
+        }
+
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+            scanbarcode(image);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void scanbarcode(ImageProxy image) {
+
+        @SuppressLint("UnsafeOptInUsageError") Image image1 = image.getImage();
+        assert image != null;
+        InputImage inputImage = InputImage.fromMediaImage(image1,image.getImageInfo().getRotationDegrees());
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE,
+                                Barcode.FORMAT_AZTEC)
+                        .build();
+
+        BarcodeScanner scanner = BarcodeScanning.getClient(options);
+        Task<List<Barcode>> result = scanner.process(inputImage)
+                .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                    @Override
+                    public void onSuccess(List<Barcode> barcodes) {
+                       readerBarcodeData(barcodes);
+
+                        // Task completed successfully
+                        // ...
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
+                    @Override
+                    public void onComplete(@NonNull Task<List<Barcode>> task) {
+                        image.close();
+                    }
+                });
+
+    }
+
+    private void readerBarcodeData(List<Barcode> barcodes) {
+        for (Barcode barcode: barcodes) {
+            Rect bounds = barcode.getBoundingBox();
+            Point[] corners = barcode.getCornerPoints();
+
+            String rawValue = barcode.getRawValue();
+
+            int valueType = barcode.getValueType();
+            // See API reference for complete list of supported types
+            switch (valueType) {
+                case Barcode.TYPE_WIFI:
+                    String ssid = barcode.getWifi().getSsid();
+                    String password = barcode.getWifi().getPassword();
+                    int type = barcode.getWifi().getEncryptionType();
+                    break;
+                case Barcode.TYPE_URL:
+                    String title = barcode.getUrl().getTitle();
+                    String url = barcode.getUrl().getUrl();
+                    break;
+            }
+        }
+
+
+    }
+
+
 }
