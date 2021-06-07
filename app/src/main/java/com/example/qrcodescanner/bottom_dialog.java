c7d4f6fb084f34e4579f8043a5c185ce63176529
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-07 23:28:15 +0600

    bottom dialog added

---
+++ app/src/main/java/com/example/qrcodescanner/bottom_dialog.java
@@ -0,0 +1,7 @@
+package com.example.qrcodescanner;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class bottom_dialog extends BottomSheetDialogFragment {
+
+}
